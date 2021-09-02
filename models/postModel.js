6a233d8dd31da769b4ed198f01878a25c10ecce7
yazan ali <yazanabuali2000@gmail.com>
2021-09-02 15:53:57 +0300

    add image upload

--- models/postModel.js
+++ models/postModel.js
@@ -1,31 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const postSchema = new mongoose.Schema({
-//     body: String,
-//     createdAt: String,
-//     image: String,
-//     comments: [{
-//         body: String,
-//         first_name: String,
-//         last_name: String,
-//         username: String,
-//         user_image: String,
-//         createdAt: String,
-//     }],
-//     likes: [{
-//         username: String,
-//     }],
-//     createdBy: {
-//         username: String,
-//         first_name: String,
-//         last_name: String,
-//         image: String,
-//     }
-// });
-
-// module.exports = mongoose.model("Post", postSchema);
-
-
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
